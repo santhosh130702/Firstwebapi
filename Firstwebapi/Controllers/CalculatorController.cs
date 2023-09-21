69a9cd4a12c7a35904924449a00a7bd8c655d68a
Santhosh Subbaiyan <santhosh.subbaiyan@gavstech.com>
2023-09-21 12:04:14 +0530

    webapi

---
+++ Firstwebapi/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firstwebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //api/calculator/Add?a=10&b=2;
+        [HttpGet("Calculator/Add")]
+        public int Add(int a ,int b)
+        {
+            return a+b;
+        }
+        //api/calculator/Subtract? a = 10 & b = 2;
+        [HttpGet("Calculator/Sum")]
+        public int Sum(int a, int b)
+        {
+            return a + b+1000;
+        }
+        [HttpPost]
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        //api/calculator/Multiply?a=10&b=2;
+
+        [HttpPut]
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        //api/calculator/Divide?=10&b=2;
+       
+        [HttpDelete]
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Firstwebapi/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firstwebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("Demo/Add")]
+        public int Add(int a, int b)
+        {
+            return a + b + 15000;
+        }
+    }
+}
