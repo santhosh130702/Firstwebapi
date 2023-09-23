ef679220aa4f963f6ee6c79932d458d43927abcb
santhosh.subbaiyan <santhosh.subbaiyan@200411LTP2830>
2023-09-23 12:11:56 +0530

    include employee

--- Firstwebapi/Controllers/EmployeeController.cs
+++ Firstwebapi/Controllers/EmployeeController.cs
@@ -18,20 +18,26 @@ namespace Firstwebapi.Controllers
         public IEnumerable<EmpViewModel> GetAllEmployees()
         {
             List<Employee> employees = _repositoryEmployee.AllEmployees();
-            var empList=(
+            var empList = (
                 from emp in employees
                 select new EmpViewModel()
                 {
-                    EmpId= emp.EmployeeId,
-                    FirstName= emp.FirstName,
-                    LastName= emp.LastName,
-                    BirthDate= emp.BirthDate,
-                    HireDate=  emp.HireDate,
-                    Title=emp.Title,
-                    City= emp.City,
-                    ReportsTo=emp.ReportsTo
-                }).ToList();    
+                    EmpId = emp.EmployeeId,
+                    FirstName = emp.FirstName,
+                    LastName = emp.LastName,
+                    BirthDate = emp.BirthDate,
+                    HireDate = emp.HireDate,
+                    Title = emp.Title,
+                    City = emp.City,
+                    ReportsTo = emp.ReportsTo
+                }).ToList();
             return empList;
+        }
+        [HttpGet("/ListAllEmployees")]
+        public List<Employee> ListAllEmployees()
+        {
+            List<Employee> employeesList = _repositoryEmployee.AllEmployees();
+            return employeesList;
         }
         [HttpGet("/FindEmployee")]
         public Employee FindEmployee(int id)
@@ -39,33 +45,71 @@ namespace Firstwebapi.Controllers
             Employee employeeById = _repositoryEmployee.FindEmployeeById(id);
             return employeeById;
         }
-        [HttpPost("/AddEmployee")]
-        public string AddEmployee(Employee newEmployee)
+        //[HttpPost("/AddEmployee")]
+        //public string AddEmployee(/*Employee*/ newEmployee)
+        //{
+        //    int employeestatus = _repositoryEmployee.AddEmployee(newEmployee);
+        //    if (employeestatus == 0)
+        //    {
+        //        return "Employee Not Added To Database Since it already exist";
+        //    }
+        //    else
+        //    {
+        //        return "Employee Added To Database";
+        //    }
+        //}
+        //[HttpPost("/AddEmployee")]
+        //public int  {
+        //            EmpId = emp.EmployeeId,
+        //            FirstName = emp.FirstName,
+        //            LastName = emp.LastName,
+        //            BirthDate = emp.BirthDate,
+        //            HireDate = emp.HireDate,
+        //            Title = emp.Title,
+        //            City = emp.City,
+        //            ReportsTo= newEmployee.ReportsTo>0? newEmployee.ReportsTo }
+        [HttpPost("/AddNewEmployees")]
+        public int AddEmployee(EmpViewModel newemp)
         {
-            int employeestatus = _repositoryEmployee.AddEmployee(newEmployee);
-            if (employeestatus == 0)
+
+            Employee employee = new Employee()
             {
-                return "Employee Not Added To Database Since it already exist";
-            }
-            else
-            {
-                return "Employee Added To Database";
-            }
+                // EmpId = emp.EmployeeId, IT WONT work
+                FirstName = newemp.FirstName,
+                LastName = newemp.LastName,
+                BirthDate = newemp.BirthDate,
+                HireDate = newemp.HireDate,
+                Title = newemp.Title,
+                City = newemp.City,
+                ReportsTo = newemp.ReportsTo > 0 ? newemp.ReportsTo : null,
+            };
+           int result= _repositoryEmployee.AddEmployee(employee);
+            return result;
         }
-        [HttpPut]
-        public Employee EditEmployee(int id, [FromBody] Employee updatedEmployee)
+
+        ////public int EditEmployee(int id, [FromBody] Employee updatedEmployee)
+        ////{
+
+        ////    updatedEmployee.EmployeeId = id; // Ensure the ID in the URL matches the EmployeeId
+
+
+
+        ////    var savedEmployee = _repositoryEmployee.UpdateEmployee(updatedEmployee);
+        ////    return savedEmployee;
+        //}
+        [HttpPut("/EditEmployee")]
+        public int EditEmployee(int id, [FromBody] EmpViewModel emp)
         {
-
-            updatedEmployee.EmployeeId = id; // Ensure the ID in the URL matches the EmployeeId
-
-
-
-            Employee savedEmployee = _repositoryEmployee.UpdateEmployee(updatedEmployee);
-            return savedEmployee;
+            Employee employee = new Employee();
+            employee.EmployeeId = emp.EmpId;
+            employee.FirstName = emp.FirstName;
+            employee.LastName = emp.LastName;
+            _repositoryEmployee.UpdateEmployee(employee);
+            return 1;
         }
-        
-        
-        [HttpGet("/DeleteEmployee")]
+
+
+        [HttpDelete("/DeleteEmployee")]
         public string DeleteEmployee(int id)
         {
             int employeestatus = _repositoryEmployee.DeleteEmployee(id);
--- Firstwebapi/Model/RepositoryEmployee.cs
+++ Firstwebapi/Model/RepositoryEmployee.cs
@@ -1,4 +1,7 @@
-﻿namespace Firstwebapi.Model
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Extensions;
+
+namespace Firstwebapi.Model
 {
     public class RepositoryEmployee
     {
@@ -18,22 +21,66 @@
         }
         public int AddEmployee(Employee newEmployee)
         {
+            Employee? foundEmp = _context.Employees.Find(newEmployee.EmployeeId);
+            if(foundEmp != null) {
+                throw new Exception("failed to add employeee");
+            }
+            EntityState es = _context.Entry(newEmployee).State;
+            Console.WriteLine($"EntityState B4Add:{es.GetDisplayName()}");
             _context.Employees.Add(newEmployee);
-            return _context.SaveChanges();
+            es = _context.Entry(newEmployee).State;
+            Console.WriteLine($"EntityState After Add:{es.GetDisplayName()} ");
+            int result= _context.SaveChanges();
+            es = _context.Entry(newEmployee).State;
+            Console.WriteLine($"EntityState After savechange:{es.GetDisplayName()} ");
+            return result;
         }
 
-        public Employee UpdateEmployee(Employee updatedEmployee)
+        //public Employee UpdateEmployee(Employee updatedEmployee)
+        //{
+        //    _context.Employees.Update(updatedEmployee);
+        //    // Console.WriteLine(_context.Entry(updatedEmployee).State); // sir,i will do it , thankyou sir
+        //    _context.SaveChanges();
+        //    return updatedEmployee;
+        //}
+        public int UpdateEmployee(Employee modifiedEmployee)
         {
-            _context.Employees.Update(updatedEmployee);
-            // Console.WriteLine(_context.Entry(updatedEmployee).State); // sir,i will do it , thankyou sir
-            _context.SaveChanges();
-            return updatedEmployee;
+            EntityState es = _context.Entry(modifiedEmployee).State;
+            Console.WriteLine($"EntityState B4update:{es.GetDisplayName()}");
+            _context.Employees.Update(modifiedEmployee);
+            es = _context.Entry(modifiedEmployee).State;
+            Console.WriteLine($"EntityState After update:{es.GetDisplayName()} ");
+            int result = _context.SaveChanges();
+            es = _context.Entry(modifiedEmployee).State;
+            Console.WriteLine($"EntityState After savechange:{es.GetDisplayName()} ");
+            return result;
         }
         public int DeleteEmployee(int id)
         {
-            Employee emp = _context.Employees.Find(id);
-            _context.Employees.Remove(emp);
-            return _context.SaveChanges();
+            Employee empToDelete = _context.Employees.Find(id);
+            EntityState es = EntityState.Detached;
+            int result = 0;
+            if (empToDelete != null)
+            {
+                es = _context.Entry(empToDelete).State;
+                Console.WriteLine($"EntityState b4update:{es.GetDisplayName()}");
+                _context.Employees.Remove(empToDelete);
+                es = _context.Entry(empToDelete).State;
+                Console.WriteLine($"EntityState after update:{es.GetDisplayName()}");
+                result = _context.SaveChanges();
+                es = _context.Entry(empToDelete).State;
+                Console.WriteLine($"EntityState after saved update:{es.GetDisplayName()}");
+
+
+
+
+
+            }
+            return result;
+
+
+
+
         }
     }
 }
